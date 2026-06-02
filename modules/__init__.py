a87a2927d4a832c4cd2b626973fd3906faa35cc6
Melody Angeline Samudio <samudiomelody99@gmail.com>
2026-06-02 18:10:42 -0500

    Fix PyInstaller: populate __init__.py + hidden-import flags for all modules

---
+++ modules/__init__.py
@@ -0,0 +1,7 @@
+from modules import tabs
+from modules import adventurerGeneration
+from modules import questGeneration
+from modules import armory
+from modules import randomEvents
+from modules import facilities
+from modules import raidBoss
