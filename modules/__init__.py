2d0b20246a3e70995443b42eb3b4928a5d625840
Melody Angeline Samudio <samudiomelody99@gmail.com>
2026-06-02 19:41:57 -0500

    Empty __init__.py (avoids circular import in PyInstaller bundle)

--- modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from modules import tabs
-from modules import adventurerGeneration
-from modules import questGeneration
-from modules import armory
-from modules import randomEvents
-from modules import facilities
-from modules import raidBoss
